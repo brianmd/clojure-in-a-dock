7bb42efd92b61dadd76ed9dda5207a3ecc545bf5
Brian Murphy-Dye <brian@murphydye.com>
2016-07-17 12:35:08 -0400

    Add repl connection.

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -ti --rm -v $(pwd -P)/..:/root/docker clojure:latest /bin/bash
+docker run -ti --rm -p 127.0.0.1:7894:7894 -v $(pwd -P)/..:/root/docker clojure:latest /bin/bash
