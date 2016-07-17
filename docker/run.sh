62313c64eac28d17260e89cfc882c9f9aef15432
Brian Murphy-Dye <brian@murphydye.com>
2016-07-17 13:11:24 -0400

    Retain .m2 and .lein directories.

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -ti --rm -p 127.0.0.1:7894:7894 -v $(pwd -P)/..:/root/docker clojure:latest /bin/bash
+# note: must be run from the docker directory
+mkdir -p m2
+mkdir -p lein
+docker run -ti --rm -p 127.0.0.1:7894:7894 -v $(pwd -P)/..:/root/docker -v $(pwd -P)/m2:/root/.m2 -v $(pwd -P)/lein:/root/.lein clojure:latest /bin/bash
